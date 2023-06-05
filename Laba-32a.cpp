443afc4ce87dd5327cd495afb4fbfb4d3d7b4061
Artem <128305840+ArtemProgers@users.noreply.github.com>
2023-06-05 18:25:53 +0300

    Add files via upload

---
+++ Laba-32a.cpp
@@ -0,0 +1,83 @@
+﻿//Laba-32a
+//В очередь Och2 поместить все латинские буквы из Och1 и найти их количество
+
+#include <iostream> //библиотеки
+#include <locale>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+using namespace std; // пространство имён
+
+typedef struct node {
+    char data;
+    struct node* next;
+} Node;
+
+typedef struct queue {
+    Node* first;
+    Node* last;
+    int size;
+} Queue;
+
+Queue* createQueue() { // функция инициализации
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    if (q) {
+        q->first = NULL;
+        q->last = NULL;
+        q->size = 0;
+    }
+    return q;
+}
+
+void enqueue(Queue* q, char data) { //функция добавления элемента
+    Node* n = (Node*)malloc(sizeof(Node));
+    if (n) {
+        n->data = data;
+        n->next = NULL;
+        if (q->last != NULL) q->last->next = n;
+        q->last = n;
+        if (q->first == NULL) q->first = n;
+        q->size++;
+    }
+
+}
+
+char dequeue(Queue* q) { //функция удаления элемента
+    if (q->first == NULL) return '\0';
+    Node* n = q->first;
+    char data = n->data;
+    q->first = n->next;
+    if (q->first == NULL) q->last = NULL;
+    free(n);
+    q->size--;
+    return data;
+}
+
+int getLatinLetters(Queue* q) { //функция подсчёта букв
+    int count = 0;
+    Node* current = q->first;
+    while (current != NULL) {
+        if (isalpha(current->data)) count++;
+        current = current->next;
+    }
+    return count;
+}
+
+void scan(Queue* q) {
+    char str[100]{};
+    cout << "Введите символы в строку без пробелов: ";
+    gets_s(str);
+    for (int i = 0; str[i] != '\0'; i++) enqueue(q, str[i]);
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian"); //руссификация текста
+
+    Queue* q = createQueue(); //инициализируем
+    
+    scan(q); //вводим символы
+    int count = getLatinLetters(q); //считаем буквы
+    cout << "Количество латинских букв: " << count; //выводим результат
+    return 0;
+}
---
+++ Laba-32b.cpp
@@ -0,0 +1,72 @@
+﻿//Laba-32b
+//В очередь Och2 поместить все латинские буквы из Och1 и найти их количество
+
+#include <iostream> //библиотеки
+#include <locale>
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX_SIZE 100 // максимальный размер очереди
+using namespace std; // пространство имён
+
+typedef struct {
+    char queue[MAX_SIZE]; // массив элементов очереди
+    int front; // индекс первого элемента
+    int rear; // индекс последнего элемента
+} Queue;
+
+void enqueue(Queue* q, char c) { // функция для добавления элемента в очередь
+    if (q->rear == MAX_SIZE - 1) {
+        cout << "Очередь полная\n";
+        return;
+    }
+    q->rear++;
+    q->queue[q->rear] = c;
+}
+
+char dequeue(Queue* q) { // функция для удаления элемента из очереди
+    if (q->front > q->rear) {
+        cout << "Очередь пустая\n";
+        return '\0';
+    }
+    char removed = q->queue[q->front];
+    q->front++;
+    return removed;
+}
+
+void scan(Queue* och1) { // функция заполнения очереди
+    cout << "Введите 10 символов в очередь Och1:" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "Символ " << i + 1 << ": ";
+        char a = 0;
+        cin >> a;
+        cout << "\n";
+        enqueue(och1, a);
+    }
+}
+
+
+void poisk(Queue och1, Queue och2) { // функция поиска символов
+    int count = 0; 
+    while (och1.front <= och1.rear) {
+        char c = dequeue(&och1);
+        if (isalpha(c)) {
+            count++;
+            enqueue(&och2, c);
+        }
+    }
+    cout << "Количество латинских букв: " << count;
+}
+
+
+
+int main() {
+    setlocale(LC_ALL, "Russian"); //руссификация текста
+
+    Queue och1 = { { }, 0, 10}; // инициализируем
+    Queue och2 = { {0}, 0, -1 }; 
+
+    scan(&och1); //вводим значения
+    poisk(och1, och2); //ищем буквы и их количество
+    return 0;
+}
